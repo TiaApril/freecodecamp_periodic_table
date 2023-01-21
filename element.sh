cf5c42d830a2e260eb0cab33061f046ac5e5cec3
TiaApril <95060757+TiaApril@users.noreply.github.com>
2023-01-22 00:34:20 +0700

    Update element.sh

--- element.sh
+++ element.sh
@@ -1,30 +1,34 @@
-#! /bin/bash
-
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --tuples-only -c"
 
-
+#if no argument
 if [[ -z $1 ]]
 then
 echo "Please provide an element as an argument."
 exit
-fi
-
-#if argument is atomic number
-if [[ $1 =~ ^[1-9]+$ ]]
-then
-  element=$($PSQL "select atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) where atomic_number = '$1'")
 else
-#if argument is string
-  element=$($PSQL "select atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) where name = '$1' or symbol = '$1'")
+  VALID_ID1=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements FULL JOIN properties USING(atomic_number) FULL JOIN types USING(type_id) WHERE atomic_number = $1")
+  VALID_ID2=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements FULL JOIN properties USING(atomic_number) FULL JOIN types USING(type_id) WHERE symbol = '$1' OR name = '$1'")
+  
+  #if it's integer
+  if [[ $VALID_ID1 ]]
+  then
+    echo $VALID_ID1 | while IFS=" |" read an name symbol type mass mp bp 
+    do
+    echo "The element with atomic number $an is $name ($symbol). It's a $type, with a mass of $mass amu. $name has a melting point of $mp celsius and a boiling point of $bp celsius."
+    done
+    else
+    #if it's string
+      if [[ $VALID_ID2 ]]
+      then
+       echo $VALID_ID2 | while IFS=" |" read an name symbol type mass mp bp 
+      do
+      echo "The element with atomic number $an is $name ($symbol). It's a $type, with a mass of $mass amu. $name has a melting point of $mp celsius and a boiling point of $bp celsius."
+      done
+      else
+      #not valid argumnent
+      # if argumnet is not atomic number
+      echo "I could not find that element in the database."
+      exit
+      fi
+  fi
 fi
-
-#element not in db
-if [[ -z $element ]]
-then
-  echo -e "\nI could not find that element in the database."
-  exit
-fi
-echo $element | while IFS=" |" read an name symbol type mass mp bp 
-do
-  echo -e "\nThe element with atomic number $an is $name ($symbol). It's a $type, with a mass of $mass amu. $name has a melting point of $mp celsius and a boiling point of $bp celsius."
-done
